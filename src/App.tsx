7a7a06de7759fb5ed4b199b7cb7694975134551b
mahdi.najafzadeh <mahdi.najafzadeh@partsoftware.com>
2026-07-27 19:13:41 +0330

    UPDATE: remove pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Home from "@/layouts/main/pages/home";
 import LoginLayout from "@/layouts/login";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    <Route index element={<Home />} />
-                </Route>
-                <Route path="/auth/login" element={<LoginLayout />} />
-            </Routes>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<Home />} />
+				</Route>
+				<Route path="/auth/login" element={<LoginLayout />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
