d0822f4ef49effa6ffd6727de9df8bc90116321e
mahdi.najafzadeh <mahdi.najafzadeh@partsoftware.com>
2026-07-27 14:37:24 +0330

    UPDATE: remove unessecery pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "@/layouts/main";
 import Home from "@/layouts/main/pages/home";
-import Users from "@/layouts/main/pages/users";
-import Settings from "@/layouts/main/pages/settings";
 import LoginLayout from "@/layouts/login";
 
 export default function App() {
@@ -11,8 +9,6 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<Home />} />
-                    <Route path="users" element={<Users />} />
-                    <Route path="settings" element={<Settings />} />
                 </Route>
                 <Route path="/auth/login" element={<LoginLayout />} />
             </Routes>
