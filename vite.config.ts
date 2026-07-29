42ea26da4c599e8758bb0e35d5fd05daccd19e61
mahdi.najafzadeh <mahdi.najafzadeh@partsoftware.com>
2026-07-29 17:14:11 +0330

    FIX: connect to backend server

--- vite.config.ts
+++ vite.config.ts
@@ -5,34 +5,41 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 3000,
-		proxy: {
-			"/api/chat/completion": {
-				target: "https://aistudio-test.partcorp.ir/api/v1/digital-employees/chat/completions",
-				changeOrigin: true,
-				rewrite: (v) => v.replace(/\/api\/chat\/completion/, ""),
-				configure: (proxy, _options) => {
-					proxy.on("proxyReq", (proxyReq) => {
-						console.log(`< ${proxyReq.method} ${proxyReq.path}`);
-						proxyReq.setHeader(
-							"X-API-KEY",
-							"sk-xVdzUjVAnIaNLEeBm0iRdOBca7jj0iSCB5AGR4f2T70",
-						);
-					});
-					proxy.on("proxyRes", (proxyReq, req, res) => {
-						console.log(
-							`> ${proxyReq.method} ${proxyReq.path} ${res.statusCode}`,
-						);
-					});
-				},
-			},
-		},
-	},
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        port: 3000,
+        proxy: {
+            "/api/chat/completion": {
+                target: "https://aistudio-test.partcorp.ir/api/v1/digital-employees/chat/completions",
+                changeOrigin: true,
+                rewrite: (v) => v.replace(/\/api\/chat\/completion/, ""),
+                configure: (proxy, _options) => {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        console.log(`< ${proxyReq.method} ${proxyReq.path}`);
+                        proxyReq.setHeader("X-API-KEY", "sk-xVdzUjVAnIaNLEeBm0iRdOBca7jj0iSCB5AGR4f2T70");
+                    });
+                    proxy.on("proxyRes", (proxyReq, req, res) => {
+                        console.log(`> ${proxyReq.method} ${proxyReq.path} ${res.statusCode}`);
+                    });
+                },
+            },
+            "/api": {
+                target: "https://help-test.partcorp.ir",
+                changeOrigin: true,
+                configure: (proxy, _options) => {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        console.log(`< ${proxyReq.method} ${proxyReq.path}`);
+                    });
+                    proxy.on("proxyRes", (proxyReq, req, res) => {
+                        console.log(`> ${proxyReq.method} ${proxyReq.path} ${res.statusCode}`);
+                    });
+                },
+            },
+        },
+    },
 });
