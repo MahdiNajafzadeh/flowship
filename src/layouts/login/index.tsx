2949e0a7021178e040830d0461ee829256604e02
mahdi.najafzadeh <mahdi.najafzadeh@partsoftware.com>
2026-07-29 17:13:05 +0330

    UPDATE: update login page

--- src/layouts/login/index.tsx
+++ src/layouts/login/index.tsx
@@ -1,3 +1,49 @@
-export default function login() {
-	return <>login</>;
+import { LoginForm } from "@/components/login-form";
+import { useAuth } from "@/hooks/use-auth";
+import { api } from "@/lib/api";
+import { cn } from "@/lib/utils";
+import { ImageDithering } from "@paper-design/shaders-react";
+import { useMutation } from "@tanstack/react-query";
+import { Suspense, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const imageList = Object.keys(import.meta.glob("../../../public/img/*", { eager: true }));
+const getImageRandom = () => imageList[Math.floor(imageList.length * Math.random())].replaceAll("../../../public", "");
+const AsyncShader = async () => (
+	<ImageDithering
+		image={getImageRandom()}
+		className="size-full"
+		colorBack="#000000"
+		colorFront="#ffffff"
+		colorHighlight="#ffffff"
+		originalColors
+		inverted={false}
+		type="8x8"
+		size={5}
+		colorSteps={5}
+		fit="cover"
+	/>
+);
+
+export function Shader(props: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div {...props} className={cn(props.className, "flex justify-center items-center")}>
+			<Suspense fallback={<span className="shimmer shimmer-color-white text-muted-foreground">Loading</span>}>
+				<AsyncShader />
+			</Suspense>
+		</div>
+	);
+}
+
+export default function Login(props: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div {...props} className={cn(props.className, "w-screen h-screen flex")}>
+			<Shader className="w-1/2 h-full" />
+			<div className="flex flex-1 items-center justify-center">
+				<div className="w-full max-w-xs">
+					<LoginForm />
+				</div>
+			</div>
+		</div>
+	);
 }
