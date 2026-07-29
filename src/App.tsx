cf06ff32f7818661da1482d8d8f7beaf5693979d
mahdi.najafzadeh <mahdi.najafzadeh@partsoftware.com>
2026-07-29 17:11:54 +0330

    UPDATE: update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "@/layouts/main";
-import Home from "@/layouts/main/pages/home";
-import LoginLayout from "@/layouts/login";
+import { lazy, Suspense } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { client } from "./lib/query";
+import { ProtectedRoute } from "@/components/protected-route";
+import { PageLoading } from "@/components/page-loading";
+
+const MainLayout = lazy(() => import("@/layouts/main"));
+const Home = lazy(() => import("@/layouts/main/pages/home"));
+const LoginLayout = lazy(() => import("@/layouts/login"));
+const NotFound = lazy(() => import("@/components/not-found-page"));
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainLayout />}>
-					<Route index element={<Home />} />
-				</Route>
-				<Route path="/auth/login" element={<LoginLayout />} />
-			</Routes>
-		</BrowserRouter>
-	);
+    return (
+        <QueryClientProvider client={client}>
+            <BrowserRouter>
+                <Suspense fallback={<PageLoading />}>
+                    <Routes>
+                        <Route element={<ProtectedRoute />}>
+                            <Route path="/" element={<MainLayout />}>
+                                <Route index element={<Home />} />
+                            </Route>
+                        </Route>
+                        <Route path="/auth/login" element={<LoginLayout />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
